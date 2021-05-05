5a3d4e62112c17417c9066218858b0f3447d52bf
UgAr-study <uglovskij.au@gmail.com>
2021-05-05 23:49:50 +0300

    first implementation

--- connected_pendulums.py
+++ connected_pendulums.py
@@ -2,66 +2,43 @@
 from __future__ import division
 import numpy as np
 from numpy import linalg as LA
-from math import sin, cos
 
 g = 9.8
 
 
-class Integrator:
-    """ Integrator for solving differential equations """
-    def __init__(self, _sum, dx):
-        """ _sum is the initial value of the target function and dx is the step
-        of the independent variable """
-        self.sum = _sum
-        self.dx = dx
+class InitState:
 
-    def __call__(self, f):
-        """ Get the area under the curve of f and accumulate for the
-        next call """
-        self.sum += self.dx * f
-        return self.sum
-
-
-class Solver:
-    """Solve for second-order differential equations"""
-    def __init__(self, A, B, C, y_dot, y):
-        self.A = A
-        self.B = B
-        self.C = C
-
-    def get_next(self):
-
-
-    def
-
-
+    def __init__(self, phi: float = 0, phi_dot: float = 1.0, m: float = 1):
+        self.phi = phi
+        self.phi_dot = phi_dot
+        self.m = m
 
 
 class ConnectedPendulums:
     """ Numerical simulation of a double pendulum system using the angles of
     the pendulums """
-    def __init__(self, dt, phi1: float=0.1, phi2: float=0.1, phi3: float=0.1,
-                 phi1_dot: float = 0.0, phi2_dot: float = 0.0, phi3_dot: float = 0.0,
-                 k1: float=1.0, k2: float=1.0,
-                 m1: float=1.0, m2: float=1.0, m3: float=1.0, l: float=1.0, betta: float=0.1):
+
+    def __init__(self, dt, pend1: InitState, pend3: InitState, pend2: InitState,
+                 k1: float = 1.0, k2: float = 1.0,
+                 l: float = 1.0, betta: float = 0.1):
         """ dt is the time-step, th10 and th20 are the initial angles, thp10 and
         thp20 are initial speeds, len is the length of the pendulums and m their
         mass """
         self.dt = dt
-        self.phi = np.array([[phi1, phi2, phi3]]).transpose()
-        self.phi_dot = np.array([[phi1_dot, phi2_dot, phi3_dot]]).transpose()
-        self.A = np.diag([m1*l**2, m2*l**2, m3*l**2])
+        self.phi = np.array([[pend1.phi, pend2.phi, pend3.phi]]).transpose()
+        self.phi_dot = np.array([[pend1.phi_dot, pend2.phi_dot, pend3.phi_dot]]).transpose()
+        self.A = np.diag([pend1.m * l ** 2, pend2.m * l ** 2, pend3.m * l ** 2])
         self.B = np.zeros((3, 3))
         self.B[2][2] = betta
-        c11 = m1*g*l + k1*l**2 / 4
-        c12 = -k1*l**2 / 4
+        c11 = pend1.m * g * l + k1 * l ** 2 / 4
+        c12 = -k1 * l ** 2 / 4
         c13 = 0
         c21 = c12
-        c22 = m2*g*l + (k1 + k2)*l**2 / 4
-        c23 = -k2*l**2 / 4
+        c22 = pend2.m * g * l + (k1 + k2) * l ** 2 / 4
+        c23 = -k2 * l ** 2 / 4
         c31 = 0
         c32 = c23
-        c33 = m3*g*l + k2*l**2 / 4
+        c33 = pend3.m * g * l + k2 * l ** 2 / 4
         self.C = np.array([[c11, c12, c13],
                            [c21, c22, c23],
                            [c31, c32, c33]])
@@ -77,7 +54,7 @@ class ConnectedPendulums:
         self.phi_dot += acc * self.dt
         self.phi += self.phi_dot * self.dt
 
-        return self.phi
+        return self.phi, self.phi_dot
 
 
 def main():
@@ -85,33 +62,61 @@ def main():
     import matplotlib.pyplot as plt
     dt = 0.1
     t = np.arange(0, 100, dt)
-    pendulums = ConnectedPendulums(dt)
+
+    pend1 = InitState(0.2, 0.1, 1)
+    pend2 = InitState(0.25, 0.01, 4)
+    pend3 = InitState(0.14, 0.15, 2)
+
+    pendulums = ConnectedPendulums(dt, pend1, pend2, pend3, 2, 3, 2, 0.4)
 
     phis1, phis2, phis3 = [], [], []
+    phis1_dot, phis2_dot, phis3_dot = [], [], []
+
     print('Simulating...')
     for ti in t:
-        phis = pendulums.get_next_state().reshape((3,))
+        phis = pendulums.get_next_state()[0].reshape((3,))
+
         phis1.append(phis[0])
         phis2.append(phis[1])
         phis3.append(phis[2])
 
+        phis_dot = pendulums.get_next_state()[1].reshape((3,))
+        phis1_dot.append(phis_dot[0])
+        phis2_dot.append(phis_dot[1])
+        phis3_dot.append(phis_dot[2])
+
     phis1 = np.array(phis1)
     phis2 = np.array(phis2)
     phis3 = np.array(phis3)
+
+    phis1_dot = np.array(phis1_dot)
+    phis2_dot = np.array(phis2_dot)
+    phis3_dot = np.array(phis3_dot)
+
     print('Finished!')
 
-    fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
+    fig, (phi1, phi1_dot, phi2, phi2_dot, phi3, phi3_dot) = plt.subplots(6, 1)
     fig.suptitle('A tale of 3 subplots')
 
-    ax1.plot(t, phis1)
-    ax1.set_ylabel('phis1')
+    phi1.plot(t, phis1)
+    phi1.set_ylabel('phi1')
 
-    ax2.plot(t, phis2)
-    ax2.set_ylabel('phis2')
+    phi1_dot.plot(t, phis1_dot)
+    phi1_dot.set_ylabel('phi1_dot')
 
-    ax3.plot(t, phis3)
-    ax3.set_ylabel('phis3')
-    ax3.set_xlabel('time (s)')
+    phi2.plot(t, phis2)
+    phi2.set_ylabel('phi2')
+
+    phi2_dot.plot(t, phis2_dot)
+    phi2_dot.set_ylabel('phi2_dot')
+
+    phi3.plot(t, phis3)
+    phi3.set_ylabel('phi3')
+
+    phi3_dot.plot(t, phis3_dot)
+    phi3_dot.set_ylabel('phi3_dot')
+
+    phi3_dot.set_xlabel('time (s)')
 
     plt.show()
 
--- render.py
+++ render.py
@@ -5,7 +5,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from draw import Point, Draw
 from connected_pendulums import ConnectedPendulums
-import pygame as pg 
+from connected_pendulums import InitState
+import pygame as pg
 import math
 
 fps = 60
@@ -34,38 +35,53 @@ susp_height = (lta_susp - p11)()[1]
 
 pend_radius = 10
 
+
 def main():
     """ Run the program """
     phi1 = float(input('phi1 (degrees): '))
+    phi1_dot = float(input('phi1_dot (degrees/sec): '))
     phi2 = float(input('phi2 (degrees): '))
+    phi2_dot = float(input('phi2_dot (degrees/sec): '))
     phi3 = float(input('phi3 (degrees): '))
-
-    #phi_dots input also
+    phi3_dot = float(input('phi3_dot (degrees/sec): '))
 
     phi1 *= math.pi / 180
     phi2 *= math.pi / 180
     phi3 *= math.pi / 180
 
+    phi1_dot *= math.pi / 180
+    phi2_dot *= math.pi / 180
+    phi3_dot *= math.pi / 180
+
     m1 = float(input('Mass 1 (kg): '))
     m2 = float(input('Mass 2 (kg): '))
     m3 = float(input('Mass 3 (kg): '))
+
+    pendulum1 = InitState(phi1, phi1_dot, m1)
+    pendulum2 = InitState(phi2, phi2_dot, m2)
+    pendulum3 = InitState(phi3, phi3_dot, m3)
+
+    k1 = float(input('k1 (N/kg): '))
+    k2 = float(input('k2 (N/kg): '))
 
     l = float(input('Length (m): '))
     betta = float(input('betta : '))
 
     dt = 1 / fps
 
-    pendulums = ConnectedPendulums(dt, phi1, phi2, phi3, m1=m1, m2=m2, m3=m3, l=l, betta=betta)
+    pendulums = ConnectedPendulums(dt, pendulum1, pendulum2, pendulum3,
+                                   k1=k1, k2=k2, l=l, betta=betta)
 
     scale = height / 2
     screen = pg.display.set_mode((width, height))
     canvas = Draw(screen, width / 2, height / 5)
-    
+
     pg.init()
     clk = pg.time.Clock()
     end = False
 
     phis1, phis2, phis3 = [phi1], [phi2], [phi3]
+    phis1_dot, phis2_dot, phis3_dot = [phi1_dot], [phi2_dot], [phi3_dot]
 
     t = [0.0]
 
@@ -74,12 +90,17 @@ def main():
             if event.type == pg.QUIT:
                 end = True
         screen.fill(white)
-        
-        phis = pendulums.get_next_state().reshape((3,))
+
+        phis = pendulums.get_next_state()[0].reshape((3,))
 
         phis1.append(phis[0])
         phis2.append(phis[1])
         phis3.append(phis[2])
+
+        phis_dot = pendulums.get_next_state()[1].reshape((3,))
+        phis1_dot.append(phis_dot[0])
+        phis2_dot.append(phis_dot[1])
+        phis3_dot.append(phis_dot[2])
 
         t.append(t[-1] + dt)
 
@@ -107,20 +128,37 @@ def main():
     phis2 = np.array(phis2)
     phis3 = np.array(phis3)
 
-    fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
-    fig.suptitle('Phi(t)')
+    phis1_dot = np.array(phis1_dot)
+    phis2_dot = np.array(phis2_dot)
+    phis3_dot = np.array(phis3_dot)
 
-    ax1.plot(t, phis1)
-    ax1.set_ylabel('phi1')
+    fig, (phi1_plt, phi1_dot_plt, phi2_plt, phi2_dot_plt,
+          phi3_plt, phi3_dot_plt) = plt.subplots(6, 1)
 
-    ax2.plot(t, phis2)
-    ax2.set_ylabel('phi2')
+    fig.suptitle('Phi and phi_dot plots')
 
-    ax3.plot(t, phis3)
-    ax3.set_ylabel('phi3')
-    ax3.set_xlabel('time (s)')
+    phi1_plt.plot(t, phis1)
+    phi1_plt.set_ylabel('phi1')
+
+    phi1_dot_plt.plot(t, phis1_dot)
+    phi1_dot_plt.set_ylabel('phi1_dot')
+
+    phi2_plt.plot(t, phis2)
+    phi2_plt.set_ylabel('phi2')
+
+    phi2_dot_plt.plot(t, phis2_dot)
+    phi2_dot_plt.set_ylabel('phi2_dot')
+
+    phi3_plt.plot(t, phis3)
+    phi3_plt.set_ylabel('phi3')
+
+    phi3_dot_plt.plot(t, phis3_dot)
+    phi3_dot_plt.set_ylabel('phi3_dot')
+
+    phi3_dot_plt.set_xlabel('time (s)')
 
     plt.show()
+
 
 if __name__ == '__main__':
     main()
